be39500768cb3e1e32a417d3388c86301721e3aa
Aqilla Az Zikra <aqilla190906@gmail.com>
2022-06-14 21:57:40 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
+
+const init = async () => {
+  server.route(routes);
+ 
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
 const server = Hapi.server({
   port: 5000,
   host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -11,11 +19,5 @@ const server = Hapi.server({
   },
 });
 
-const init = async () => {
-  server.route(routes);
- 
-  await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
-};
 
 init();
