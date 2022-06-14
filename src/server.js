b0472290380fcfefe57c390da632c944d8e262f6
Aqilla Az Zikra <aqilla190906@gmail.com>
2022-06-14 22:06:29 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -11,11 +11,9 @@ const server = Hapi.server({
   },
 });
 
-const init = async () => {
+
   server.route(routes);
-  
-  await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
-};
+
 
 init();
